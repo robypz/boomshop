6bb8ba9759253678a437918cd262714ae94c95be
robypz <robertyepez0208@hotmail.com>
2023-11-29 14:08:12 -0400

    11292023

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,11 @@
         <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="{{ asset('images/banners/ffbanner1.png') }}" class="d-block w-100" alt="...">
+                    <img src="{{ asset('assets/images/banners/ffbanner1.png') }}" class="d-block w-100" alt="...">
                 </div>
 
                 <div class="carousel-item">
-                    <img src="{{ asset('images/banners/gibanner.png') }}" class="d-block w-100" alt="...">
+                    <img src="{{ asset('assets/images/banners/gibanner.png') }}" class="d-block w-100" alt="...">
                 </div>
 
             </div>
