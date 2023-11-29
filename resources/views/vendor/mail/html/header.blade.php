b75f0ad84ecfcce627d6518fd1aec0e532cc7dfc
robypz <robertyepez0208@hotmail.com>
2023-11-29 13:20:29 -0400

    11292023-1:20

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,7 +4,7 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-<img src="{{asset('images/LOGO COMPLETO TRANSP.png')}}" class="logo" alt="BOOMSHOP Logo">
+<img src="{{asset('assets/images/LOGO COMPLETO TRANSP.png')}}" class="logo" alt="BOOMSHOP Logo">
 <br>
 {{ $slot }}
 @endif
