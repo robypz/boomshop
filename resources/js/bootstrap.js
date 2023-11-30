ee644ec07aae8ad907c5653033a04f95f9ee288e
robypz <robertyepez0208@hotmail.com>
2023-11-30 14:43:25 -0400

    11302023-1423

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -42,7 +42,7 @@ window.Echo = new Echo({
     forceTLS: false
 });*/
 
-import Echo from 'laravel-echo';
+/*import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
@@ -56,4 +56,17 @@ window.Echo = new Echo({
     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
+});*/
+
+import Echo from 'laravel-echo';
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: 'e0eb5a0f9f031e309149',
+    cluster: 'mt1',
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    wssPort: 443
 });
