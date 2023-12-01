7808fe140e202fed57e749680753d5eef98ae351
robypz <robertyepez0208@hotmail.com>
2023-12-01 10:45:14 -0400

    12-1-2023-1045

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,8 +39,12 @@ return [
                 'host' => env('PUSHER_HOST', 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => false,
+                'encrypted' => true,
                 'useTLS' => false,
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ]
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
