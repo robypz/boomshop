57a7e7e6c7aab85276a67dfabaf8f925edff8f4f
robypz <robertyepez0208@hotmail.com>
2023-12-01 16:22:20 -0400

    1212023-1622

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -52,9 +52,11 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
     //wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: true,
+    //wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    //wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
     //forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
+    wsPort: 6001,
+    disableStats: true,
+    forceTLS: true
 });
