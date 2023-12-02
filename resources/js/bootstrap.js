68ad61d55d8cf086f87d125b3494dd8e34ce7d7e
robypz <robertyepez0208@hotmail.com>
2023-12-02 12:09:00 -0400

    1222023-1208

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -57,4 +57,5 @@ window.Echo = new Echo({
     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
     disableStats: false,
+    encrypted: true,
 });
