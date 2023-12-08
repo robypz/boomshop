f0e1c24ccf9e0571187535483925722006282ec3
root <root@ubuntu-cloudpanel.localhost>
2023-12-08 04:01:09 +0000

    12082023-0000

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -45,7 +45,7 @@ return [
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
-                ],
+                ]
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
