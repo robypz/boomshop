2709f8ffde2b069ad98b24b289a1de2d2cb25f70
robypz <robertyepez0208@hotmail.com>
2023-12-08 17:19:52 -0400

    12082023-1719

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -41,7 +41,7 @@ return [
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME','https'),
                 'encrypted' => true,
-                'useTLS' => false,
+                'useTLS' => true,
                 /*'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
