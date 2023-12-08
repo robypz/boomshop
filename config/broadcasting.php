d780c1856afc96fc3a02920854d6fa3162bcd723
robypz <robertyepez0208@hotmail.com>
2023-12-08 13:33:41 -0400

    12082023

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,16 +38,16 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 //'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-                'host' => "127.0.0.1",
+                'host' => "isboomshop.com",
                 //'port' => env('PUSHER_PORT', 443),
-                'port' => 6001,
-                'scheme' => "http",
-                'encrypted' => false,
-                'useTLS' => false,
-                /*'curl_options' => [
+                'port' => 443,
+                'scheme' => "https",
+                'encrypted' => true,
+                'useTLS' => true,
+                'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
-                ]*/
+                ]
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
