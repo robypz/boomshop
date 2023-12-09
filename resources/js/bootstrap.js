acc8f515d479a6f496ae74c71e8b2706e310e803
robypz <robertyepez0208@hotmail.com>
2023-12-09 12:13:53 -0400

    12092023

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -51,7 +51,7 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: window.location.hostname,
+    wsHost: import.meta.env.VITE_PUSHER_HOST,
     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
     forceTLS: true,
