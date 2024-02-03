b3332b8cdad4df35237e3176c8939e8e20ed72be
robypz <robertyepez0208@hotmail.com>
2024-02-03 19:12:10 -0400

    Test de Binance Pay API

--- app/Http/Controllers/BinanceController.php
+++ app/Http/Controllers/BinanceController.php
@@ -24,21 +24,25 @@ class BinanceController extends Controller
     {
         $timestamp = time() * 1000;
         $nonce = bin2hex(random_bytes(16));
-        $body = json_encode(array(
-            "env" => array(
-                "terminalType" => "APP"
-            ),
-            "merchantTradeNo" => mt_rand(982538, 9825382937292),
+        $body = json_encode([
+            "env" => [
+                "terminalType" => "WEB"
+            ],
+            "orderTags" => [
+                "ifProfitSharing" => true
+            ],
+            "merchantTradeNo" => "98253829372924243",
             "orderAmount" => 25.17,
             "currency" => "USDT",
-            "goods" => array(
+            "description" => "very good Ice Cream",
+            "goodsDetails" => [
                 "goodsType" => "01",
                 "goodsCategory" => "D000",
                 "referenceGoodsId" => "7876763A3B",
                 "goodsName" => "Ice Cream",
                 "goodsDetail" => "Greentea ice cream cone"
-            )
-        ),true);
+            ]
+        ],true);
 
         $payload = $timestamp . "\n" . $nonce . "\n" . $body . "\n";
         $signature = strtoupper(bin2hex(hash_hmac("sha512", $payload, config('app.binancePayApiSecret'), true)));
