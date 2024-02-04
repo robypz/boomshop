c2c08dc7d1e5d0b7b29f7eb1e06dd9d943821974
robypz <robertyepez0208@hotmail.com>
2024-02-04 11:32:38 -0400

    Test de Binance Pay API

--- app/Http/Controllers/BinanceController.php
+++ app/Http/Controllers/BinanceController.php
@@ -24,75 +24,40 @@ class BinanceController extends Controller
     {
         $timestamp = time() * 1000;
         $nonce = bin2hex(random_bytes(16));
-        $body = array(
-            "env" => array(
+        $body = [
+            "env" => [
                 "terminalType" => "WEB"
-            ),
-            "orderTags" => array(
+            ],
+            "orderTags" => [
                 "ifProfitSharing" => true
-            ),
+            ],
             "merchantTradeNo" => "98253829372924243",
             "orderAmount" => 25.17,
             "currency" => "USDT",
             "description" => "very good Ice Cream",
-            "goodsDetails" => array(
+            "goodsDetails" => [
                 "goodsType" => "01",
                 "goodsCategory" => "D000",
                 "referenceGoodsId" => "7876763A3B",
                 "goodsName" => "Ice Cream",
                 "goodsDetail" => "Greentea ice cream cone"
-            )
-        );
+            ]
+        ];
 
-        $payload = $timestamp . "\n" . $nonce . "\n" . '{
-            "env": {
-              "terminalType": "APP"
-            },
-            "orderTags": {
-              "ifProfitSharing": true
-            },
-            "merchantTradeNo": "9825382937292",
-            "orderAmount": 25.17,
-            "currency": "USDT",
-            "description": "very good Ice Cream",
-            "goodsDetails": [{
-              "goodsType": "01",
-              "goodsCategory": "D000",
-              "referenceGoodsId": "7876763A3B",
-              "goodsName": "Ice Cream",
-              "goodsDetail": "Greentea ice cream cone"
-            }]
-          }' . "\n";
-        $signature = strtoupper(hex2bin(hash_hmac("sha512", $payload, config('app.binancePayApiSecret'))));
+        $payload = $timestamp . "\n" . $nonce . "\n" . json_encode($body) . "\n";
+        $signature = strtoupper(hash_hmac("SHA512", $payload, config('app.binancePayApiSecret')));
 
         $reponse =  $this->binance->post('order', [
             'headers' => [
                 'content-type' => 'application/json',
+                'Accept' => 'application/json',
                 'BinancePay-Timestamp' => $timestamp,
                 'BinancePay-Nonce' => $nonce,
                 'BinancePay-Certificate-SN' => config('app.binancePayApiKey'),
                 'BinancePay-Signature' => $signature,
             ],
 
-            'body'  => '{
-                "env": {
-                  "terminalType": "APP"
-                },
-                "orderTags": {
-                  "ifProfitSharing": true
-                },
-                "merchantTradeNo": "9825382937292",
-                "orderAmount": 25.17,
-                "currency": "USDT",
-                "description": "very good Ice Cream",
-                "goodsDetails": [{
-                  "goodsType": "01",
-                  "goodsCategory": "D000",
-                  "referenceGoodsId": "7876763A3B",
-                  "goodsName": "Ice Cream",
-                  "goodsDetail": "Greentea ice cream cone"
-                }]
-              }',
+            'body'  => json_encode($body),
         ]);
 
         print_r($reponse);
