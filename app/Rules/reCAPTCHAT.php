aaf2babe50c3878c3c1b04a451de5c9d4223c1ea
Robert Yepez <robertyepez0208@hotmail.com>
2024-04-26 00:12:59 -0400

    test de reCAPTCHA

--- app/Rules/reCAPTCHAT.php
+++ app/Rules/reCAPTCHAT.php
@@ -20,7 +20,7 @@ class reCAPTCHAT implements ValidationRule
             'response' => $value,
         ]);
 
-        dd($response->json());
+        dd($response,config('app.reCaptchaSecretKey'));
 
         /*if () {
             $fail('El reCAPTCHA no es válido');
